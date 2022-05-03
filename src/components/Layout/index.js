b6adeb58c407b42a50a90136030a0aaffa86f14d
jamesikjo <jamesikjo@gmail.com>
2022-05-02 19:13:57 -0700

    change Layout/Main to Layout/MainLayout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-export { default as Main } from "./Main";
+export { default as MainLayout } from "./MainLayout";
 export { default as ProjectDetailLayout } from "./ProjectDetailLayout";
 
 //import in pages/portfolio/[singleProject]
