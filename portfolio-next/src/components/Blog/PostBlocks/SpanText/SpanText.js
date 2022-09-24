976125b691e52e2f019e3a62e4d9b8f55e283092
jamesikjo <jamesikjo@gmail.com>
2022-09-23 21:31:34 -0700

    Move SpanText component to separate file

---
+++ portfolio-next/src/components/Blog/PostBlocks/SpanText/SpanText.js
@@ -0,0 +1,42 @@
+import Typography from "@mui/material/Typography";
+
+const annotationStyles = (value, props) => {
+  const {
+    annotations: { bold, code, color, italic, strikethrough, underline },
+  } = value;
+
+  return {
+    ...(bold && { fontWeight: 500 }),
+    ...(props.component.includes("h") && { fontWeight: 600 }),
+    color: color === "default" ? `${props.muiColor}.main` : color,
+    fontStyle: italic && "italic",
+    textDecoration: `${underline ? "underline" : ""} ${
+      strikethrough ? "line-through" : ""
+    }`,
+  };
+};
+
+const SpanText = (props) => {
+  const { text, variant, component, id } = props;
+  if (!text) return;
+
+  return text.map((value, i) => {
+    return (
+      <Typography
+        key={id + i}
+        variant={variant}
+        component={component}
+        sx={annotationStyles(value, props)}
+      >
+        {value.text.link ? (
+          <a href={value.text.link.url} target="_blank">
+            {value.text.content}
+          </a>
+        ) : (
+          value.text.content
+        )}
+      </Typography>
+    );
+  });
+};
+export default SpanText;
