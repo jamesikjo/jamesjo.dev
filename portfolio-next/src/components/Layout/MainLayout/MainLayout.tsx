2840b9074ae63c1ec0931896648d2128b7584bf6
jamesikjo <jamesikjo@gmail.com>
2022-11-01 09:42:54 -0700

    Add in meta data

--- portfolio-next/src/components/Layout/MainLayout/MainLayout.tsx
+++ portfolio-next/src/components/Layout/MainLayout/MainLayout.tsx
@@ -1,12 +1,8 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { Box } from "@mui/material";
 import TopBar from "./TopBar/";
 import Footer from "./Footer";
-
-interface MainLayoutProps {
-  children?: React.ReactNode;
-  title?: string;
-}
 
 const NAV_LINKS = [
   { title: "About", path: "/about" },
@@ -15,11 +11,44 @@ const NAV_LINKS = [
   { title: "Contact", path: "mailto:jamesjodev@gmail.com" },
 ];
 
+const meta = {
+  title: "James Jo | Web Developer",
+  description: "My work portfolio and blog",
+  image: "/meta-image.png",
+  type: "website",
+};
+
+interface MainLayoutProps {
+  children?: React.ReactNode;
+  title?: string;
+}
+
 const MainLayout = ({ children, title }: MainLayoutProps) => {
+  const router = useRouter();
+
   return (
     <>
       <Head>
-        <title>{title}</title>
+        <title>{title || meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <link rel="canonical" href={`https://jamesjo.dev${router.asPath}`} />
+        <meta
+          property="og:url"
+          content={`https://jamesjo.dev${router.asPath}`}
+        />
+        <meta property="og:title" content={title || meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:site_name" content="jamesjo.dev" />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:image" content={meta.image} />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content={`https://jamesjo.dev${router.asPath}`}
+        />
+        <meta property="twitter:title" content={title || meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:image" content={meta.image} />
       </Head>
       <Box
         sx={{
