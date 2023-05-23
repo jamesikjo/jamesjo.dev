62ad694ba50be34534e1fe91df4a9e05c3b00b8b
jamesikjo <jamesikjo@gmail.com>
2023-05-23 13:20:23 -0700

    Update global styles + MUI global theme

--- portfolio-next/styles/globalTheme.ts
+++ portfolio-next/styles/globalTheme.ts
@@ -1,107 +1,65 @@
 import { createTheme, css } from "@mui/material/styles";
-import { blue } from "@mui/material/colors";
-import { PaletteMode } from "@mui/material";
 
-const globalTheme = (mode: PaletteMode) =>
-  createTheme({
-    palette: {
-      mode,
-      ...(mode === "light"
-        ? {
-            primary: {
-              light: "#ededed",
-              main: "#2b2b2b",
-              dark: "#777777",
-            },
-            secondary: {
-              main: "#5c5c5c",
-              light: "#bababa",
-            },
-            info: {
-              main: "#2196f3",
-            },
-            background: {
-              default: "#F9FAFB",
-            },
-          }
-        : {
-            primary: {
-              light: "#ededed",
-              main: "#F2F5F7",
-              dark: "#777777",
-            },
-            secondary: {
-              main: "#B2B9C0",
-              light: "#bababa",
-            },
-            info: {
-              main: "#2196f3",
-            },
-            text: {
-              primary: "#2b2b2b",
-            },
-            background: {
-              default: "#0E141B",
-              paper: "#182635",
-            },
-          }),
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#ededed",
+      main: "#d1d1d1",
+      dark: "#777777",
     },
-    typography: {
-      fontFamily: `"Poppins"`,
-      h6: {
-        fontSize: "1.3em",
-      },
-      subtitle1: {
-        lineHeight: 1.85,
-      },
-      subtitle2: {
-        lineHeight: 1.85,
-      },
+    secondary: {
+      main: "#5c5c5c",
+      light: "#bababa",
     },
-    components: {
-      MuiIconButton: {
-        styleOverrides: {
-          root: {
-            "&:hover": {
-              backgroundColor: "transparent",
-              color: blue[500],
-            },
-          },
-        },
-      },
-      MuiButton: {
-        defaultProps: {
-          disableRipple: true,
-        },
-        styleOverrides: {
-          root: {
-            borderRadius: "3px",
-          },
-          text: {
-            "&:hover": {
-              backgroundColor: "transparent",
-              color: blue[500],
-            },
+    background: {
+      default: "#d1d1d1",
+    },
+  },
+  typography: {
+    fontFamily: `"Montserrat"`,
+    subtitle1: {
+      fontSize: "15px",
+    },
+  },
+  components: {
+    MuiIconButton: {
+      styleOverrides: {
+        root: {
+          "&:hover": {
+            backgroundColor: "transparent",
+            color: "#fff",
           },
         },
       },
     },
-  });
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true,
+      },
+      styleOverrides: {
+        text: {
+          "&:hover": {
+            backgroundColor: "transparent",
+            color: "#fff",
+          },
+        },
+      },
+    },
+  },
+});
 
 export const globalStyles = css`
   :root {
     body {
-      background-color: #f9fafb;
-      color: #5c5c5c;
+      background-color: #1d1d20;
     }
   }
 
   [data-theme="dark"] {
     body {
-      background-color: #0e141b;
-      color: #b2b9c0;
+      background-color: #1d1d20;
     }
   }
 `;
 
-export default globalTheme;
+export default theme;
