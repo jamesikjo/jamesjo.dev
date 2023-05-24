cbc44cd3aaa6653b979cfa5e8ee671af8cd295b4
jamesikjo <jamesikjo@gmail.com>
2023-05-24 12:24:12 -0700

    Remove unused collection and component types

--- portfolio-strapi/src/api/latest/controllers/latest.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  latest controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::latest.latest');
--- portfolio-strapi/src/api/latest/routes/latest.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * latest router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::latest.latest');
